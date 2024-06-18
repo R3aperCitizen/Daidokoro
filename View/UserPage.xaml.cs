34684c7314a98abfff03ac316df17e1b7632896b
R3aperCitizen <justr3aper3@gmail.com>
2024-06-18 19:49:10 +0200

    Fixed spacing

--- View/UserPage.xaml.cs
+++ View/UserPage.xaml.cs
@@ -15,6 +15,6 @@ public partial class UserPage : ContentPage
 
     protected override void OnAppearing()
     {
-        UserInfo.ItemsSource = _globals.GetUtente(2);
+        UserInfo.ItemsSource = _globals.GetUtente(1);
     }
 }
