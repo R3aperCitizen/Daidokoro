b501c7b0e452f080051b5186d6acdf22317b28fc
Tommaso De Tommaso <tommaso.detommaso@protonmail.com>
2024-06-20 12:36:30 +0200

    Updated InfoButton to allow Clicked event handling, updated UserPage to use that event on click

--- View/UserPage.xaml.cs
+++ View/UserPage.xaml.cs
@@ -15,6 +15,21 @@ public partial class UserPage : ContentPage
 
     protected override void OnAppearing()
     {
-        UserInfo.ItemsSource = _globals.GetUserById(2);
+        UserInfo.ItemsSource = _globals.GetUserById(1);
+    }
+
+    private async void LikesInfoButton_Clicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Likes", "Premuto!", "OK");
+    }
+
+    private async void ReviewsInfoButton_Clicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Recensioni", "Premuto!", "OK");
+    }
+
+    private async void RecipesInfoButton_Clicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Ricette", "Premuto!", "OK");
     }
 }
