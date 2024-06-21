53a608bf95fd090ded165d2e19d76a47882d8206
Tommaso De Tommaso <tommaso.detommaso@protonmail.com>
2024-06-21 11:09:00 +0200

    Components are now completely modular, requiring their own attributes in XAML.

--- View/Controls/RecipeFull.xaml.cs
+++ View/Controls/RecipeFull.xaml.cs
@@ -1,15 +1,98 @@
-namespace Daidokoro.View.Controls;
+﻿namespace Daidokoro.View.Controls;
 
 public partial class RecipeFull : ContentView
 {
-	public RecipeFull()
+    private const string DIFFICULTY_SYMBOL = "🌶️";
+    private const string LIKES_SYMBOL = "❤️";
+    private const string TIME_UNIT = "min";
+
+    public static readonly BindableProperty IdProperty =
+        BindableProperty.Create(nameof(Id), typeof(int), typeof(RecipeFull));
+    public new int Id
+    {
+        get => (int)GetValue(IdProperty);
+        set => SetValue(IdProperty, value);
+    }
+
+    public static readonly BindableProperty TitleProperty =
+        BindableProperty.Create(nameof(Title), typeof(string), typeof(RecipeFull), propertyChanged: OnTitleChanged);
+    private static void OnTitleChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        var control = (RecipeFull)bindable;
+        control.titleLabel.Text = (string)newValue;
+    }
+    public string Title
+    {
+        get => (string)GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+
+    public static readonly BindableProperty TimeProperty =
+        BindableProperty.Create(nameof(Time), typeof(int), typeof(RecipeFull), propertyChanged: OnTimeChanged);
+    private static void OnTimeChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        var control = (RecipeFull)bindable;
+        control.timeLabel.Text = $"{(int)newValue} {TIME_UNIT}";
+    }
+    public int Time
+    {
+        get => (int)GetValue(TimeProperty);
+        set => SetValue(TimeProperty, value);
+    }
+
+    public static readonly BindableProperty DifficultyProperty =
+        BindableProperty.Create(nameof(Difficulty), typeof(int), typeof(RecipeFull), propertyChanged: OnDifficultyChanged);
+    private static void OnDifficultyChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        var control = (RecipeFull)bindable;
+        control.difficultyLabel.Text = Enumerable.Repeat(DIFFICULTY_SYMBOL, (int)newValue)
+                                                 .Aggregate((s1, s2) => s1 + s2);
+    }
+    public int Difficulty
+    {
+        get => (int)GetValue(DifficultyProperty);
+        set => SetValue(DifficultyProperty, value);
+    }
+
+    public static readonly BindableProperty LikesProperty =
+        BindableProperty.Create(nameof(Likes), typeof(int), typeof(RecipeFull), propertyChanged: OnLikesChanged);
+    private static void OnLikesChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        var control = (RecipeFull)bindable;
+        control.likesLabel.Text = $"{(int)newValue} {LIKES_SYMBOL}";
+    }
+    public int Likes
+    {
+        get => (int)GetValue(LikesProperty);
+        set => SetValue(LikesProperty, value);
+    }
+
+    public static readonly BindableProperty ImageProperty =
+        BindableProperty.Create(nameof(Image), typeof(byte[]), typeof(RecipeFull), propertyChanged: OnImageChanged);
+    private static void OnImageChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        var control = (RecipeFull)bindable;
+        control.imageBlob.Source = (byte[])newValue;
+    }
+    public byte[] Image
+    {
+        get => (byte[])GetValue(ImageProperty);
+        set => SetValue(ImageProperty, value);
+    }
+
+    public RecipeFull()
 	{
 		InitializeComponent();
-	}
+
+        titleLabel.Text = "null";
+        timeLabel.Text = "null";
+        difficultyLabel.Text = "null";
+        likesLabel.Text = "null";
+    }    
 
     private async void GoToRecipePage(object sender, EventArgs e)
     {
         Button button = (Button)sender;
-        await Shell.Current.GoToAsync($"//{nameof(RecipePage)}?IdRicetta={button.AutomationId}");
+        await Shell.Current.GoToAsync($"//{nameof(RecipePage)}?IdRicetta={Id}");
     }
 }
