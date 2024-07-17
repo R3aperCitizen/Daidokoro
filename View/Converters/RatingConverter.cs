1aca1253c131f06606b6d9dc82b2f594c9877aec
Tommaso De Tommaso <tommaso.detommaso@protonmail.com>
2024-07-17 18:30:47 +0200

    New Controls are now also used in Ratings to fix a problem with the emoji

---
+++ View/Converters/RatingConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daidokoro.View.Converters
+{
+    public class RatingConverter : IValueConverter
+    {
+        private const string POSITIVE_VOTE = "🍋‍🟩";
+        private const string NEGATIVE_VOTE = "🧅";
+
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+            => (bool)value ? POSITIVE_VOTE : NEGATIVE_VOTE;
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
