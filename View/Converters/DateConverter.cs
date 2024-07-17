d3b14f442f2fd565b6a1ced4603d4ec620c0ef18
Tommaso De Tommaso <tommaso.detommaso@protonmail.com>
2024-07-17 18:12:26 +0200

    Updated DateConverter to a one-liner

--- View/Converters/DateConverter.cs
+++ View/Converters/DateConverter.cs
@@ -13,16 +13,7 @@ namespace Daidokoro.View.Converters
         private const string FAILURE_STRING = "Non ottenuto";
 
         public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-        {
-            string result = FAILURE_STRING;
-
-            if(value is DateTime date)
-            {
-                result = date.ToString(DATE_FORMAT);
-            }
-
-            return result;
-        }
+            => value is DateTime ? ((DateTime)value).ToString(DATE_FORMAT) : FAILURE_STRING;
 
         public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
             => throw new NotImplementedException();
